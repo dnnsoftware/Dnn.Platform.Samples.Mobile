bd02574e3385b5ce7781a8201688d6f399cc5415
Miguel Gatmaytan <miguel.gatmaytan@dnnsoftware.com>
2016-01-05 09:39:20 -0800

    Removal of unused code.

--- app/js/app.js
+++ app/js/app.js
@@ -1,25 +1,6 @@
 (function () {
     var app;
 
-    window.Books = {
-        data: new kendo.data.DataSource({
-            transport: {
-                read: {
-                    url: "data/books.js",
-                    type: "get",
-                    dataType: "json"
-                }
-            },
-        schema: {
-            data: "books"
-        }
-    }),
-        back: function() {
-        },
-        settings: function() {
-        }
-    };
-
     document.addEventListener("deviceready", function () {  
         navigator.splashscreen.hide();
 
